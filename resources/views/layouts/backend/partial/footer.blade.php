dc46615fd6d77f1dd37153edec49c64397f470af
DESKTOP-OQ5ORD0\sajib <sajib.k.mridha@gmail.com>
2020-01-27 19:53:29 +0600

    admin & author dashboard and functionality update

---
+++ resources/views/layouts/backend/partial/footer.blade.php
@@ -0,0 +1,46 @@
+<!-- Jquery Core Js -->
+<script src="{{ asset('assets/backend/plugins/jquery/jquery.min.js') }}"></script>
+
+<!-- Bootstrap Core Js -->
+<script src="{{ asset('assets/backend/plugins/bootstrap/js/bootstrap.js') }}"></script>
+
+<!-- Select Plugin Js -->
+<script src="{{ asset('assets/backend/plugins/bootstrap-select/js/bootstrap-select.js') }} "></script>
+
+<!-- Slimscroll Plugin Js -->
+<script src="{{ asset('assets/backend/plugins/jquery-slimscroll/jquery.slimscroll.js') }}"></script>
+
+<!-- Waves Effect Plugin Js -->
+<script src="{{ asset('assets/backend/plugins/node-waves/waves.js') }}"></script>
+
+<!-- Jquery CountTo Plugin Js -->
+<script src="{{ asset('assets/backend/plugins/jquery-countto/jquery.countTo.js') }}"></script>
+
+<!-- Morris Plugin Js -->
+<script src="{{ asset('assets/backend/plugins/raphael/raphael.min.js') }}"></script>
+<script src="{{ asset('assets/backend/plugins/morrisjs/morris.js') }}"></script>
+
+<!-- ChartJs -->
+<script src="{{ asset('assets/backend/plugins/chartjs/Chart.bundle.js') }}"></script>
+
+<!-- Flot Charts Plugin Js -->
+<script src="{{ asset('assets/backend/plugins/flot-charts/jquery.flot.js') }}"></script>
+<script src="{{ asset('assets/backend/plugins/flot-charts/jquery.flot.resize.js') }}"></script>
+<script src="{{ asset('assets/backend/plugins/flot-charts/jquery.flot.pie.js') }}"></script>
+<script src="{{ asset('assets/backend/plugins/flot-charts/jquery.flot.categories.js') }}"></script>
+<script src="{{ asset('assets/backend/plugins/flot-charts/jquery.flot.time.js') }}"></script>
+
+<!-- Sparkline Chart Plugin Js -->
+<script src="{{ asset('assets/backend/plugins/jquery-sparkline/jquery.sparkline.js') }}"></script>
+
+<!-- Custom Js -->
+<script src="{{ asset('assets/backend/js/admin.js') }}"></script>
+<script src="{{ asset('assets/backend/js/pages/index.js') }}"></script>
+
+<!-- Demo Js -->
+<script src="{{ asset('assets/backend/js/demo.js') }} "></script>
+
+@stack('js');
+</body>
+
+</html>
